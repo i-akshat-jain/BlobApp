578c5f166f1c3daa303b9a7f640e3a2b95f47409
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2023-07-28 19:00:53 +0100

    Implementation done

--- blobCS/blobApp/models.py
+++ blobCS/blobApp/models.py
@@ -1,23 +1,25 @@
+from django.db.models import F
 from django.db import models
 from django.utils import timezone
 
 class Blob(models.Model):
     id = models.AutoField(primary_key=True)
     blob = models.CharField(max_length=100, unique=True,
-                            null=False, default=None)
-    date = models.DateField(default = timezone.now)
-
+                            null=False)
+    date = models.DateField(default=timezone.now)
+    time = models.TimeField(default=timezone.now().time())
     def __str__(self):
-        return f"{self.blob} - {self.date}"
+        return f"{self.blob} - {self.date} - {self.time}"
 
 
 class Mention(models.Model):
     id = models.AutoField(primary_key=True)
-    blob_id = models.ForeignKey(Blob, on_delete=models.CASCADE, default=None)
-    rank = models.IntegerField( default=None)
-    mention = models.CharField(max_length=255, default=None)
-    url = models.URLField( default=None)
+    blob_id = models.ForeignKey(Blob, db_column='blob_id', on_delete=models.CASCADE, default=None)
+    mention = models.CharField(max_length=500, null=False, default=None)
+    position = models.IntegerField(default = 0, null=True)
     wikipedia_url = models.URLField()
 
     def __str__(self):
-        return f"{self.mention} - {self.url}"
+        return f"{self.mention} - {self.wikipedia_url} - {self.position} - {self.blob_id}"
+
+
--- blobCS/blobApp/views.py
+++ blobCS/blobApp/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from blobApp.models import Mention as m
 from blobApp.models import Blob as b
-# from blobApp.serializers import mentionSerializer as ser
-# from blobApp.serializers import blobSerializer as bser
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from django_filters.rest_framework import filters
 import os
 import pandas as pd
 from datetime import date
@@ -21,19 +15,27 @@ from django.conf import settings
 
 
 def get_data_and_save(blob_name):
-    account_url = "https://casestudy01.blob.core.windows.net/"
-    container_name = "wikipedia"
-
+    account_url = "https://invenicscasestudy.blob.core.windows.net/"
+    container_name = "wiki"
+    # clear all previous records form the mention and blob table
+    m.objects.all().delete()
+    # b.objects.all().delete()
+    
+    blob_name = blob_name+'.gz'
     try:
         # Get data from Azure Blob Storage and save to database
         blob_service_client = bs(account_url=account_url)
         container_client = blob_service_client.get_container_client(
             container_name)
-        blob_list = container_client.list_blobs()
-        mention_id = 1
-        idForBlobTable = 1
+        # Convert blob_list to a list to access the blobs
+        blob_list = list(container_client.list_blobs())
 
-        for blob in blob_list:
+        # blob_list = container_client.list_blobs()
+        mention_id = 1
+        idForBlobTable = 0
+        x=1
+        # blob_list = list(blob_list)
+        for blob in blob_list:  
             if blob.name == blob_name:
                 print(blob.name, idForBlobTable)
 
@@ -43,20 +45,24 @@ def get_data_and_save(blob_name):
                 # Uncompress the data
                 compressed_data = BytesIO(blob_contents.content_as_bytes())
                 unzipped_data = gzip.GzipFile(fileobj=compressed_data).read()
-                f = BytesIO(unzipped_data)
-
-                blob_obj = b.objects.create(
-                    id=idForBlobTable, blob=blob.name, date=date.today())
+                byte_file = BytesIO(unzipped_data)
+                
+                if b.objects.filter(blob=blob.name).exists():
+                    print("object exists")
+                else:
+                    # idForBlobTable += 1
+                    blob_obj = b.objects.create(id=idForBlobTable, blob=blob.name, date=date.today(), time = datetime.datetime.now().time())
                 mentionObject = []
                 rank = 1
 
-                for line in f:
+                for line in byte_file:
                     fields = line.decode('utf-8').strip().split('\t')
                     if fields[0] == 'MENTION':
                         mention = fields[1]
-                        target_url = fields[2]
+                        position = fields[2]
+                        wiki_url = fields[3]
                         mentionObject.append(
-                            m(id=mention_id, blob_id=blob_obj, rank=rank, mention=mention, url=target_url))
+                            m(id=mention_id, blob_id=blob_obj, mention=mention, position=position,wikipedia_url=wiki_url))
 
                         # Create bulk insert having 3000 records each time
                         if rank % 3000 == 0:
@@ -68,72 +74,37 @@ def get_data_and_save(blob_name):
                         mention_id += 1
                         rank += 1
 
-                # to save the last list of leftover objects
+                # to save the last list of leftover objectsz
                 m.objects.bulk_create(mentionObject)
+        
+            else:
+                print("blob not found")
                 idForBlobTable += 1
 
     except Exception as e:
         print(e)
 
-    # Fetch data from the local extracted file
-    extracted_file_path = os.path.join(settings.MEDIA_ROOT, f'{blob_name}.txt')
-    if os.path.exists(extracted_file_path):
-        # File already exists, read the data from the file
-        with open(extracted_file_path, 'r', encoding='utf-8') as f:
-            encoded_data = f.read()
-    else:
-        # File does not exist, download and extract from Azure Blob Storage
-        download_url = f'{account_url}{container_name}/{blob_name}.gz'
-        response = requests.get(download_url)
-        if response.status_code == 200:
-            # Save the downloaded .gz file to a temporary location
-            temp_file_path = os.path.join(settings.MEDIA_ROOT, 'temp.gz')
-            with open(temp_file_path, 'wb') as f:
-                f.write(response.content)
 
-            # Extract the .gz file
-            with gzip.open(temp_file_path, 'rb') as gz_file, open(extracted_file_path, 'wb') as extracted_file:
-                extracted_file.write(gz_file.read())
+    return True
 
-            # Delete the temporary .gz file
-            os.remove(temp_file_path)
 
-            # Read the data from the extracted file
-            with open(extracted_file_path, 'r', encoding='utf-8') as f:
-                encoded_data = f.read()
-
-    # Convert the data to a list of dictionaries
-    mentions = []
-    for line in encoded_data.splitlines():
-        mention_parts = line.split('\t')
-        if len(mention_parts) >= 3:
-            mention_data = {
-                'URL': mention_parts[0],
-                'MENTION': mention_parts[1],
-                'Wikipedia URL': mention_parts[2]
+def mention_list(request):
+    mentions = m.objects.all()
+    data = {
+        'mentions': [
+            {
+                'mention': mention.mention,
+                'position': mention.position,
+                'wikipedia_url': mention.wikipedia_url,
+                'blob_id': mention.blob_id.id,
             }
-            mentions.append(mention_data)
-
-    if mentions:
-        # Save the data in PostgreSQL
-        for mention_data in mentions:
-            mention_object = m(
-                mention=mention_data['MENTION'], url=mention_data['URL'], wikipedia_url=mention_data['Wikipedia URL'])
-            mention_object.save()
-
-        # Save the file in the media folder for future reference (if not already saved)
-        if not os.path.exists(extracted_file_path):
-            with open(extracted_file_path, 'w', encoding='utf-8') as f:
-                for mention_data in mentions:
-                    f.write(
-                        f"{mention_data['URL']}\t{mention_data['MENTION']}\t{mention_data['Wikipedia URL']}\n")
-
-        return True
-
-    return False
+            for mention in mentions
+        ]
+    }
+    return JsonResponse(data)
 
 
-# @api_view(['GET', 'POST'])
+
 def home(request):
     context = {}
     if request.method == 'POST':
@@ -146,36 +117,77 @@ def home(request):
             else:
                 context['message'] = 'Failed to get data from Azure.'
 
+     # Make an HTTP GET request to the mention_list API
+    try:
+        response = requests.get('http://127.0.0.1:8000/api/mention/')
+        data = response.json()
+        mentions = data.get('mentions', [])
+        context['mentions'] = mentions
+    except requests.RequestException:
+        context['message'] = 'Failed to fetch data from the API.'
+
     return render(request, 'blobApp/home.html', context)
 
 
+    # # Fetch data from the local extracted file
+    # extracted_file_path = os.path.join(settings.MEDIA_ROOT, f'{blob_name}.txt')
+    # if os.path.exists(extracted_file_path):
+    #     # File already exists, read the data from the file
+    #     with open(extracted_file_path, 'r', encoding='utf-8') as f:
+    #         encoded_data = f.read()
+    # else:
+    #     # File does not exist, download and extract from Azure Blob Storage
+    #     download_url = f'{account_url}{container_name}/{blob_name}.gz'
+    #     response = requests.get(download_url)
+    #     if response.status_code == 200:
+    #         # Save the downloaded .gz file to a temporary location
+    #         temp_file_path = os.path.join(settings.MEDIA_ROOT, 'temp.gz')
+    #         with open(temp_file_path, 'wb') as f:
+    #             f.write(response.content)
 
-# def home(request):
-#     context = {}
-#     if request.method == 'POST':
-#         blob_name = request.POST.get('blob_name')
-#         if blob_name:
-#             # Get the data from Azure
-#             mentions = get_data_from_azure(blob_name)
+    #         # Extract the .gz file
+    #         with gzip.open(temp_file_path, 'rb') as gz_file, open(extracted_file_path, 'wb') as extracted_file:
+    #             extracted_file.write(gz_file.read())
 
-#             if mentions:
-#                 # Save the data in PostgreSQL
-#                 for mention_data in mentions:
-#                     mention_object = m(
-#                         mention=mention_data['MENTION'], url=mention_data['URL'], wikipedia_url=mention_data['Wikipedia URL'])
-#                     mention_object.save()
+    #         # Delete the temporary .gz file
+    #         os.remove(temp_file_path)
 
-#                 # Save the file in the media folder for future reference (if not already saved)
-#                 extracted_file_path = os.path.join(
-#                     settings.MEDIA_ROOT, f'{blob_name}.txt')
-#                 if not os.path.exists(extracted_file_path):
-#                     with open(extracted_file_path, 'w', encoding='utf-8') as f:
-#                         for mention_data in mentions:
-#                             f.write(
-#                                 f"{mention_data['URL']}\t{mention_data['MENTION']}\t{mention_data['Wikipedia URL']}\n")
+    #         # Read the data from the extracted file
+    #         with open(extracted_file_path, 'r', encoding='utf-8') as f:
+    #             encoded_data = f.read()
 
-#                 context['message'] = 'Data stored successfully!'
-#             else:
-#                 context['message'] = 'Failed to get data from Azure.'
+    # # Convert the data to a list of dictionaries
+    # mentions = []
+    # for line in encoded_data.splitlines():
+    #     mention_parts = line.split('\t')
+    #     if len(mention_parts) >= 3:
+    #         mention_data = {
+    #             'URL': mention_parts[0],
+    #             'MENTION': mention_parts[1],
+    #             'Wikipedia URL': mention_parts[2]
+    #         }
+    #         mentions.append(mention_data)
 
-#     return render(request, 'blobApp/home.html', context)
+    # if mentions:
+    #     # Save the data in PostgreSQL
+    #     for mention_data in mentions:
+    #         mention_object = m(
+    #             mention=mention_data['MENTION'], url=mention_data['URL'], wikipedia_url=mention_data['Wikipedia URL'])
+    #         mention_object.save()
+
+    #     # Save the file in the media folder for future reference (if not already saved)
+    #     if not os.path.exists(extracted_file_path):
+    #         with open(extracted_file_path, 'w', encoding='utf-8') as f:
+    #             for mention_data in mentions:
+    #                 f.write(
+    #                     f"{mention_data['URL']}\t{mention_data['MENTION']}\t{mention_data['Wikipedia URL']}\n")
+
+    #     return True
+
+    # return False
+
+
+# @api_view(['GET', 'POST'])
+
+
+
